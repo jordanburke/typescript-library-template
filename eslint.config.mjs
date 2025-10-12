c96d7ccacc74b256eb41afb6c376a88da685d953
Jordan Burke <jordan.burke@gmail.com>
2025-10-12 11:18:00 -0400

    Bump

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,13 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin"
-import simpleImportSort from "eslint-plugin-simple-import-sort"
-import prettier from "eslint-plugin-prettier"
-import globals from "globals"
-import tsParser from "@typescript-eslint/parser"
 import path from "node:path"
 import { fileURLToPath } from "node:url"
-import js from "@eslint/js"
+
 import { FlatCompat } from "@eslint/eslintrc"
+import js from "@eslint/js"
+import typescriptEslint from "@typescript-eslint/eslint-plugin"
+import tsParser from "@typescript-eslint/parser"
+import prettier from "eslint-plugin-prettier"
+import simpleImportSort from "eslint-plugin-simple-import-sort"
+import globals from "globals"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
